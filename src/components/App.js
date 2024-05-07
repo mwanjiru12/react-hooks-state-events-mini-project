4ec6ed4037648cac9a3b3a29f1a30df7e9ef8b53
Maria Kamau <maria.kamau@student.moringaschool.com>
2024-05-07 14:07:53 +0300

    "Add state management and event handling to enable adding and deleting tasks, and filtering tasks by category"
    This commit includes the following changes:
    
    * In `App.js`, the component now manages the state of tasks using

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const handleAddTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleAddTask} />
+      <TaskList tasks={tasks} onDeleteTask={handleDeleteTask} />
     </div>
   );
 }
